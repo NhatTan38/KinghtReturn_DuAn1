abf9b58eb53ee1fe121a6f8f37807aab08acba06
NgocToan33 <ngongoctoan2005nt@gmail.com>
2024-07-02 23:38:10 +0700

    xây dựng thêm platform, background, viết code player di chuyển, nhảy, animation

---
+++ The_Knight_Comeback/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    private Rigidbody2D _rigidbody2D;
+    Vector2 moveInput;
+    private Animator _animator;
+    CircleCollider2D _circleCollider2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _circleCollider2D = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(">>>Move Input: " + moveInput);
+    }
+
+    void OnJump(InputValue value)
+    {
+        var isTouchingGround = _circleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        if (!isTouchingGround) return;
+        if (value.isPressed)
+        {
+            _rigidbody2D.velocity += new Vector2(x: 0, y: jumpSpeed);
+        }
+
+       
+    }
+
+    // điều khiển chuyển động nhân vật
+    void Run()
+    {
+        var moveVelocity = new Vector2(moveInput.x * moveSpeed, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = moveVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(moveInput.x) > Mathf.Epsilon;
+        _animator.SetBool("isDichuyen", playerHasHorizontalSpeed);
+    }
+
+    // xoay hướng nhân vật theo chiều chuyển động 
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidbody2D.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(_rigidbody2D.velocity.x), y:1f);
+        }
+    }
+}

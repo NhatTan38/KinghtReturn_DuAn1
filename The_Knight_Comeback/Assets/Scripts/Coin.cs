a379148d60cfa7cfc85da4b8c640266198cac254
NhatTan38 <dinhnhattan.la03082005@gmail.com>
2024-07-23 16:04:21 +0700

    Hoàn thành coin và 1 số lỗi nhỏ đã fix

---
+++ The_Knight_Comeback/Assets/Scripts/Coin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Tăng điểm số hoặc làm bất kỳ hành động nào khác bạn muốn khi player thu thập coin
+            Destroy(gameObject);
+        }
+    }
+}

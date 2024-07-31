e529e24d5e5f1f651a69bd42e78fce4389d5e5f9
NhatTan38 <dinhnhattan.la03082005@gmail.com>
2024-07-31 20:23:08 +0700

    chỉnh sửa code player ( FAIL )

--- The_Knight_Comeback/Assets/Scripts/PlayerMovement.cs
+++ The_Knight_Comeback/Assets/Scripts/PlayerMovement.cs
@@ -1,67 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
+﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float jumpSpeed = 10f;
-    private Rigidbody2D _rigidbody2D;
-    Vector2 moveInput;
-    private Animator _animator;
-    CircleCollider2D _circleCollider2D;
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    private bool isJumping = false;
+    private bool isAttacking = false;
 
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool isGrounded;
+
     void Start()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
-        _circleCollider2D = GetComponent<CircleCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Run();
-        FlipSprite();
-    }
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-    void OnMove(InputValue value)
-    {
-        moveInput = value.Get<Vector2>();
-        Debug.Log(">>>Move Input: " + moveInput);
-    }
-
-    void OnJump(InputValue value)
-    {
-        var isTouchingGround = _circleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"));
-        if (!isTouchingGround) return;
-        if (value.isPressed)
+        if (moveInput != 0)
         {
-            _rigidbody2D.velocity += new Vector2(x: 0, y: jumpSpeed);
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
         }
 
-       
-    }
-
-    // điều khiển chuyển động nhân vật
-    void Run()
-    {
-        var moveVelocity = new Vector2(moveInput.x * moveSpeed, _rigidbody2D.velocity.y);
-        _rigidbody2D.velocity = moveVelocity;
-
-        bool playerHasHorizontalSpeed = Mathf.Abs(moveInput.x) > Mathf.Epsilon;
-        _animator.SetBool("isDichuyen", playerHasHorizontalSpeed);
-    }
-
-    // xoay hướng nhân vật theo chiều chuyển động 
-    void FlipSprite()
-    {
-        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidbody2D.velocity.x) > Mathf.Epsilon;
-        if (playerHasHorizontalSpeed)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            transform.localScale = new Vector2(Mathf.Sign(_rigidbody2D.velocity.x), y:1f);
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            isAttacking = true;
+            animator.SetTrigger("Attack");  // Sử dụng SetTrigger thay vì SetBool
+        }
+
+        // Kiểm tra nếu nhân vật đang trên mặt đất
+        if (Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            isGrounded = true;
+            isJumping = false;
+            animator.SetBool("isJumping", false);
+        }
+        else
+        {
+            isGrounded = false;
         }
     }
 }

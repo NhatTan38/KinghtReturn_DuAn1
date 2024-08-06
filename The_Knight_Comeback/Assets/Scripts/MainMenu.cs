e1c69c15481235a32b9733ce672b12159f464b29
NhatTan38 <dinhnhattan.la03082005@gmail.com>
2024-08-06 14:50:44 +0700

    MainMenu

---
+++ The_Knight_Comeback/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Tải Scene bằng Build Index
+        SceneManager.LoadScene(0); // Thay 1 bằng Build Index của Scene bạn muốn load
+
+        // Hoặc tải Scene bằng tên
+        // SceneManager.LoadScene("GameScene"); // Thay "GameScene" bằng tên Scene bạn muốn load
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
